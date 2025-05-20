99e4333d15d52725b34e5e6f8e6c53aa080174da
nialwrt <nialwrt1@gmail.com>
2025-05-21 07:49:57 +0800

    Update aw1k-nialwrt-firmware-update.sh

--- aw1k-nialwrt-firmware-update.sh
+++ aw1k-nialwrt-firmware-update.sh
@@ -16,7 +16,7 @@ case "$CHOICE" in
   1) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/IMMORTALWRT-24.10.1-FREE.bin" ;;
   2) URL="http://abidarwi.sh/nialwrt11052025.sh" ;;
   3) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/NEVERMORESSH.bin" ;;
-  4) URL="-" ;;
+  4) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/QWRT-V1.bin" ;;
   5) URL="-" ;;
   *) echo "CANCELLED."; exit 0 ;;
 esac
