983cf3aef2987e7c515816c834c58f1a358100a8
nialwrt <nialwrt1@gmail.com>
2025-05-21 08:04:15 +0800

    Update aw1k-nialwrt-firmware-update.sh

--- aw1k-nialwrt-firmware-update.sh
+++ aw1k-nialwrt-firmware-update.sh
@@ -4,20 +4,18 @@ echo "###########################"
 echo "AW1K NIALWRT FIRMWARE UPDATE"
 echo "###########################"
 echo "1) IMMORTALWRT 24.10.1 FREE"
-echo "2) IMMORTALWRT 24.10.1 PRO (script)"
-echo "3) NEVERMORESSH"
-echo "4) QWRT V1"
-echo "5) QWRT V2"
+echo "2) NEVERMORESSH"
+echo "3) QWRT V1"
+echo "4) QWRT V2"
 echo "###########################"
-printf "ENTER YOUR CHOICE [1-5]: "
+printf "ENTER YOUR CHOICE [1-4]: "
 read CHOICE
 
 case "$CHOICE" in
   1) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/IMMORTALWRT-24.10.1-FREE.bin" ;;
-  2) URL="http://abidarwi.sh/nialwrt11052025.sh" ;;
-  3) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/NEVERMORESSH.bin" ;;
-  4) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/QWRT-V1.bin" ;;
-  5) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/QWRT-V2.bin" ;;
+  2) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/NEVERMORESSH.bin" ;;
+  3) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/QWRT-V1.bin" ;;
+  4) URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/QWRT-V2.bin" ;;
   *) echo "CANCELLED."; exit 0 ;;
 esac
 
