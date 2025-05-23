3f82c9ac4c3abec684a3f5b7c230b39813f757a8
nialwrt <nialwrt1@gmail.com>
2025-05-23 09:26:59 +0800

    Update aw1k-nialwrt-ota-update.sh

--- aw1k-nialwrt-ota-update.sh
+++ aw1k-nialwrt-ota-update.sh
@@ -35,7 +35,7 @@ IS_PREMIUM=false
 
 case "$CHOICE" in
   1)
-    URL="https://github.com/nialwrt/AW1K-NIALWRT-FIRMWARE-UPDATE/releases/download/AW1K-FIRMWARE/IMMORTALWRT-24.10.1-LITE.bin"
+    URL="https://github.com/nialwrt/AW1K-NIALWRT-OTA-UPDATE/releases/download/AW1K-FIRMWARE/IMMORTALWRT-24.10.1-LITE.bin"
     ;;
   2)
     URL="-"
